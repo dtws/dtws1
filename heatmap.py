0c0756a72a9a274942cf99efae2962977599cecb
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-02-20 21:09:16 +0900

    複数に分割したヒートマップを作成できるようになった

--- heatmap.py
+++ heatmap.py
@@ -1,3 +1,4 @@
+import itertools as it
 import matplotlib.pyplot as plt
 from h3 import h3
 import pandas as pd
@@ -7,6 +8,7 @@ import tilemapbase as tmb
 from colormath.color_objects import sRGBColor, HSVColor
 from colormath.color_conversions import convert_color
 import folium
+from collections import namedtuple
 
 
 def _flatten(lss):
@@ -20,7 +22,10 @@ def _swap(ls):
     ]
 
 
-def extent(ids):
+def _extent(ids):
+    RetExtent = namedtuple(
+        "RetExtent", "extent min_lng max_lng min_lat max_lat")
+
     ids_u = ids.unique()
     latlngs = _flatten(map(h3.h3_to_geo_boundary, ids_u))
     lats = [x[0] for x in latlngs]
@@ -30,7 +35,50 @@ def extent(ids):
         min(lats), max(lats)
     )
     extent.to_aspect(1)
-    return extent
+    return RetExtent(extent, min(lngs), max(lngs), min(lats), max(lats))
+
+
+def extent(ids):
+    return _extent(ids).extent
+
+
+def n_extent(ids, n):
+    extent_ = _extent(ids)
+    lng_width = (extent_.max_lng - extent_.min_lng) / n
+    lat_width = (extent_.max_lat - extent_.min_lat) / n
+    lng_ticks = [
+        extent_.min_lng + i * lng_width
+        for i in range(n+1)
+    ]
+    lat_ticks = [
+        extent_.min_lat + i * lat_width
+        for i in range(n+1)
+    ]
+    MinMax = namedtuple("MinMax", "min max")
+    lng_bounds = map(lambda x: MinMax(*x), zip(lng_ticks[:-1], lng_ticks[1:]))
+    lat_bounds = map(lambda x: MinMax(*x), zip(lat_ticks[:-1], lat_ticks[1:]))
+
+    Bound = namedtuple("Bound", "longitude latitude")
+    bounds = list(map(lambda x: Bound(*x), it.product(lng_bounds, lat_bounds)))
+    extents = [
+        tmb.Extent.from_lonlat(
+            bound.longitude.min, bound.longitude.max,
+            bound.latitude.min, bound.latitude.max
+        ) for bound in bounds
+    ]
+
+    def search_position(id):
+        lat, lng = h3.h3_to_geo(id)
+        xpos = int((lng - extent_.min_lng) // lng_width)
+        ypos = int((lat - extent_.min_lat) // lat_width)
+        # import pdb
+        # pdb.set_trace()
+        return n*xpos + ypos
+    search_position_v = np.vectorize(search_position)
+    positions = search_position_v(ids)
+
+    Return = namedtuple("Return", "extents positions")
+    return Return(extents, positions)
 
 
 def color_selector(values, n, hue=0.9, max_value=None):
