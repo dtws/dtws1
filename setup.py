2394cce521cdc805c85e7da47ccedf74164d6f53
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-05-20 19:02:50 +0900

    requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,18 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["matplotlib", "pandas"]
+requirements = [
+    "colormath",
+    "cumplot",
+    "folium",
+    "geojson",
+    "geopy",
+    "h3",
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "tilemapbase"
+]
 
 setup_requirements = []
 
