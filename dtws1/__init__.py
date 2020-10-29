9b9bfd76c55a44c73c8dbfafaa2d1eb2ca7ca03c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-29 19:16:13 +0900

    timeutil

--- dtws1/__init__.py
+++ dtws1/__init__.py
@@ -5,6 +5,9 @@ __email__ = 'yasunori_horikoshi@datawise.co.jp'
 __version__ = '0.4.0'
 
 
+print("hoge")
+
 from dtws1.plotutil import cumplot
 from dtws1 import heatmap
 from dtws1 import colorutil
+from dtws1 import timeutil
