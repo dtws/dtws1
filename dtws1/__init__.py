4bdd3209c877a2649110d0106a11570ea174ea12
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-29 19:17:45 +0900

    fix

--- dtws1/__init__.py
+++ dtws1/__init__.py
@@ -5,8 +5,6 @@ __email__ = 'yasunori_horikoshi@datawise.co.jp'
 __version__ = '0.4.0'
 
 
-print("hoge")
-
 from dtws1.plotutil import cumplot
 from dtws1 import heatmap
 from dtws1 import colorutil
